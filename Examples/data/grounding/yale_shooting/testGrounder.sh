e44410acebb554f4b05a392bc814b8a5beebd1e3
Anastasios Skarlatidis <a.skarlatidis@gmail.com>
2014-11-08 12:31:14 +0200

    Updated logging configuration; added colour output for console logger; enabled file writer. Minor update to gournding testing scenario (yale_shooting example).

--- Examples/data/grounding/yale_shooting/testGrounder.sh
+++ Examples/data/grounding/yale_shooting/testGrounder.sh
@@ -8,8 +8,8 @@ CWA="Happens/2,Next/2"
 OWA="InitiatedAt/2,TerminatedAt/2"
 QUERY="HoldsAt/2"
 
-mrfwriter -i $infile -e $evfile -o $resultFile \
-	-f GROUND_CNF \
+mrfwriter -i $infile -e $evfile -o $outputFile \
+	-f "GROUND_CNF" \
 	-q $QUERY \
 	-cwa $CWA \
 	-owa $OWA
