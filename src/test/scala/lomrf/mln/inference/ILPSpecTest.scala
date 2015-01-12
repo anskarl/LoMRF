7f9baa3de9a0ef3154150c75f8532e5f1b45a392
Vagelis M <vagmcs@gmail.com>
2015-01-12 18:36:23 +0200

    ILP Specification Test (under development), currently not working

---
+++ src/test/scala/lomrf/mln/inference/ILPSpecTest.scala
@@ -0,0 +1,99 @@
+/*
+ * o                        o     o   o         o
+ * |             o          |     |\ /|         | /
+ * |    o-o o--o    o-o  oo |     | O |  oo o-o OO   o-o o   o
+ * |    | | |  | | |    | | |     |   | | | |   | \  | |  \ /
+ * O---oo-o o--O |  o-o o-o-o     o   o o-o-o   o  o o-o   o
+ *             |
+ *          o--o
+ * o--o              o               o--o       o    o
+ * |   |             |               |    o     |    |
+ * O-Oo   oo o-o   o-O o-o o-O-o     O-o    o-o |  o-O o-o
+ * |  \  | | |  | |  | | | | | |     |    | |-' | |  |  \
+ * o   o o-o-o  o  o-o o-o o o o     o    | o-o o  o-o o-o
+ *
+ * Logical Markov Random Fields.
+ *
+ * Copyright (C) 2012  Anastasios Skarlatidis.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package lomrf.mln.inference
+
+import lomrf.logic.AtomSignature
+import lomrf.mln.grounding.MRFBuilder
+import lomrf.mln.model.MLN
+import org.scalatest.{Matchers, FunSpec}
+import lomrf.util.Utilities.io.{findFiles, strToFile}
+
+/**
+ * Specification test for ILP algorithm used for MAP inference.
+ *
+ * @author Anastasios Skarlatidis
+ * @author Vagelis Michelioudakis
+ */
+final class ILPSpecTest extends FunSpec with Matchers {
+
+/*  private val sep = System.getProperty("file.separator")
+  private val testFilesPath = System.getProperty("user.dir") + sep + "Examples" + sep + "data" + sep +
+    "tests" + sep + "inference" + sep
+
+  private val mlnFiles = findFiles(strToFile(testFilesPath), f => f.getName.contains(".mln"))
+  private val dbFilesList = findFiles(strToFile(testFilesPath), f => f.getName.contains(".db"))
+
+  describe("Caviar diagonal newton test in path: '" + testFilesPath + "'") {
+
+    for(weightType <- List("HI", "SI", "SI_h")) {
+      for (fold <- 0 to 9) {
+        val mlnFile = mlnFiles.filter(f => f.getAbsolutePath.contains("fold_" + fold) &&
+          f.getAbsolutePath.contains(sep + weightType + sep))
+        val dbFiles = dbFilesList.filter(f => f.getAbsolutePath.contains("fold_" + fold) &&
+          f.getAbsolutePath.contains(sep + weightType + sep))
+
+        for(db <- dbFiles) {
+          describe("MLN from file '" + mlnFile(0) + "' with evidence from file '" + db) {
+            val mln = MLN(
+              mlnFileName = mlnFile(0).getAbsolutePath,
+              evidenceFileName = db.getAbsolutePath,
+              queryAtoms = Set(AtomSignature("HoldsAt", 2)),
+              cwa = Set(AtomSignature("Happens", 2), AtomSignature("Close", 4), AtomSignature("Next", 2),
+                AtomSignature("OrientationMove", 3), AtomSignature("StartTime", 1)))
+
+            info("Found " + mln.formulas.size + " formulas")
+            info("Found " + mln.constants.size + " constant types")
+            info("Found " + mln.schema.size + " predicate schemas")
+            info("Found " + mln.functionSchema.size + " function schemas")
+
+            describe("Creating MRF from previous MLN") {
+
+              info("Creating MRF...")
+              val mrfBuilder = new MRFBuilder(mln)
+              val mrf = mrfBuilder.buildNetwork
+
+              info("Created " + mrf.numberOfAtoms + " ground atoms")
+              info("Created " + mrf.numberOfConstraints + " ground clauses")
+
+              describe("Running MAP inference using ILP") {
+                val solver = new ILP(mrf, ilpSolver = 1)
+                solver.infer()
+              }
+            }
+          }
+        }
+
+      }
+    }
+  }*/
+}
